fc2228658a65444a00bdd46e418bcec461183342
Jethro <jethrotiago@hotmail.com>
2022-12-25 21:34:29 -0300

    Creating elements

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,24 @@ import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+//Pages
+import Home from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+   path: "/",
+   element: <App />,
+   children: [
+    {
+      path: "/",
+      element: <Home />,
+    }
+   ] 
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
