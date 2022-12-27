5af6f3b157fbefc031b5b96ea6bea9cf3a93c62e
Jethro <jethrotiago@hotmail.com>
2022-12-26 21:15:50 -0300

    Using fetch api

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,6 +6,12 @@ import { useState } from "react";
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async(userName: string) => {
+
+      const res = await fetch(`https://api.github.com/users/${userName}`)
+
+    }
     
   return (
   <div>
