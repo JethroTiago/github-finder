e4bea23b8db4c9e8fb8384f840a0e8bdb28e28ce
Jethro <jethrotiago@hotmail.com>
2022-12-26 21:06:59 -0300

    Importing UserProps from types

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,11 @@
+import { UserProps } from "../types/user";
+
 import Search from "../components/Search";
 
 import { useState } from "react";
 
 const Home = () => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState<UserProps | null>(null);
     
   return (
   <div>
