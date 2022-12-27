6a8c3b392f36d0015c423870d65beeb89c5c3484
Jethro <jethrotiago@hotmail.com>
2022-12-26 21:33:40 -0300

    Making search work

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,14 +8,16 @@ const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
 
     const loadUser = async(userName: string) => {
-
       const res = await fetch(`https://api.github.com/users/${userName}`)
 
-    }
+      const data = await res.json();
+
+      console.log(data);
+    };
     
   return (
   <div>
-    <Search />
+    <Search loadUser={ loadUser } />
   </div>
   );
 };
